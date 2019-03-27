316521c7c22a02c60a4959f913c9a46a39b3c810
Nikita Makarov <mesaverde228@gmail.com>
2019-03-28 00:09:48 +0300

    Gretta 667

--- deploy-azure.sh
+++ deploy-azure.sh
@@ -80,7 +80,7 @@ echo PHP deployment
   
 # 1. KuduSync
 if [[ "$IN_PLACE_DEPLOYMENT" -ne "1" ]]; then
-  "$KUDU_SYNC_CMD" --perf -v 5000 -f "$DEPLOYMENT_SOURCE" -t "$DEPLOYMENT_TARGET" -n "$NEXT_MANIFEST_PATH" -p "$PREVIOUS_MANIFEST_PATH" -i ".git;.hg;.deployment;"
+  "$KUDU_SYNC_CMD" -v 5000 -f "$DEPLOYMENT_SOURCE" -t "$DEPLOYMENT_TARGET" -n "$NEXT_MANIFEST_PATH" -p "$PREVIOUS_MANIFEST_PATH" -i ".git;"
   exitWithMessageOnError "Kudu Sync failed"
 else
   echo "IN_PLACE_DEPLOYMENT setted to true"
